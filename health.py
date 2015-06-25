abd4966fbea56975cbafb3f66b171b55aad6c6f4
JScott <jvscott@gmail.com>
2015-06-24 20:26:12 -0700

    Adding an upper bound to data collection

--- health.py
+++ health.py
@@ -13,6 +13,11 @@ def initialize_database(file_name):
         database['index'] = 0
     return database
 
+def increment_index(database, sample_size):
+    database['index'] += 1
+    if database['index'] >= sample_size:
+        database['index'] = 0
+
 def is_valid(utf8_content):
     """Was the request fulfilled as expected?"""
     return 'Magnificent!' in utf8_content.decode("utf-8")
@@ -30,23 +35,24 @@ def check(url, caching_enabled):
     seconds_elapsed = time.time() - start
     return (is_valid(content), seconds_elapsed)
 
-def store(data, database):
+def store(data, database, sample_size):
     """Store the results in the given database connection"""
     data_point = {'was_up': data[0], 'response_time': data[1]}
-    index = database['index']
-    database[str(index)] = data_point
-    print(index, ':', data_point)
-    database['index'] = index + 1
+    current_index = database['index']
+    database[str(current_index)] = data_point
+    print(current_index, ':', data_point)
+    increment_index(database, sample_size)
 
 @click.command()
 @click.option('--interval', default=1.0, help='Seconds between pings.')
 @click.option('--caching_enabled', is_flag=True, help='Are responses cached?')
-def daemon(interval, caching_enabled):
+@click.option('--sample_size', default=10, help='The max data point count.')
+def daemon(interval, caching_enabled, sample_size):
     """A loop for health checking"""
     database = initialize_database('data_points.db')
     while True:
         data = check(URL, caching_enabled)
-        store(data, database)
+        store(data, database, sample_size)
         time.sleep(interval)
 
 if __name__ == '__main__':
