fdb5910cc46669362cba210d98aaea6dd4cd51cf
JScott <jvscott@gmail.com>
2015-06-24 19:03:19 -0700

    Using httplib2 to actually ping the server
    We can't use Python's native urllib because it'll throw an error on a 500 status code. The server always returns a 500, even when we get a Magnificent! response.

--- health.py
+++ health.py
@@ -1,15 +1,29 @@
 #!/usr/local/bin/python3
 
 import click
+import httplib2
+
+def is_healthy(content):
+    """Was the request fulfilled as expected?"""
+    return True
+    # return 'Magnificent!' in content
+
+def request(url):
+    """Send a GET request to the given URL"""
+    http = httplib2.Http(".cache")
+    return http.request(url, "GET")
 
 @click.command()
 @click.option('--frequency', default=1, help='Seconds between pings.')
-@click.option('--url', default="localhost:12345", help='URL to ping.')
-
-def check(frequency, url):
-    """The worst health checker"""
-    print(frequency)
-    print(url)
+@click.option('--url', default="http://localhost:12345/", help='URL to ping.')
+@click.option('--timeout', default=10, help='Seconds to wait for a response.')
+def check(frequency, url, timeout):
+    """Not a great health checker"""
+    response, content = request(url)
+    if is_healthy(content):
+        print("good!")
+    else:
+        print("bad!")
 
 if __name__ == '__main__':
     check()
