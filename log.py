8212e14c93dc29fbb43a32977495f371bfc27466
JScott <jvscott@gmail.com>
2015-06-24 21:30:23 -0700

    log.py now logs to file for our poor sysadmin

--- log.py
+++ log.py
@@ -4,27 +4,28 @@ import shelve
 import click
 
 DATABASE_FILE = 'data_points.db'
+LOG_FILE = 'data_points.csv'
 
-def write_row(table, data_point):
-    row = [data_point['was_up'], data_point['response_time']]
-    table.add_row(row)
+def write_entry(data_point, log):
+    log.write('http://localhost:12345,' + str(data_point['was_up']) + ',' + str(data_point['response_time']) + '\n')
 
 @click.command()
 @click.option('--refresh', default=1.0, help='Seconds between checking data.')
 def daemon(refresh):
     """A loop for data display"""
     database = shelve.open(DATABASE_FILE, flag='r')
+    log = open(LOG_FILE, 'w')
     while True:
         index = 0
-        previous_data_points = {}
+        previous_data_points = []
         while index < database['sample_size']:
             if str(index) not in database:
                 break
             data_point = database[str(index)]
-            
+            if data_point not in previous_data_points:
+                write_entry(data_point, log)
+                previous_data_points.append(data_point)
             index += 1
-        system('clear')
-        print(previous_data_points)
         time.sleep(refresh)
 
 if __name__ == '__main__':
