2591d63e766f9c56e8eab5e860479b9db9c1b16f
JScott <jvscott@gmail.com>
2015-06-24 20:10:50 -0700

    Store everything into a database, filesystem for now

--- health.py
+++ health.py
@@ -1,7 +1,15 @@
 #!/usr/local/bin/python3
 import time
+import shelve
 import click
 import httplib2
+
+def initialize_database(file_name):
+    """Set up the database connection"""
+    database = shelve.open(file_name)
+    if 'index' not in database:
+        database['index'] = 0
+    return database
 
 def is_valid(utf8_content):
     """Was the request fulfilled as expected?"""
@@ -20,15 +28,24 @@ def check(url, caching_enabled):
     seconds_elapsed = time.time() - start
     return (is_valid(content), seconds_elapsed)
 
+def store(data, database):
+    """Store the results in the given database connection"""
+    data_point = {'was_up': data[0], 'response_time': data[1]}
+    index = database['index']
+    database[str(index)] = data_point
+    print(index, ':', data_point)
+    database['index'] = index + 1
+
 @click.command()
 @click.option('--frequency', default=1.0, help='Seconds between pings.')
 @click.option('--url', default="http://localhost:12345/", help='URL to ping.')
 @click.option('--caching_enabled', is_flag=True, help='Are responses cached?')
 def daemon(frequency, url, caching_enabled):
     """A loop for health checking"""
+    database = initialize_database('data_points.db')
     while True:
-        is_up, response_time = check(url, caching_enabled)
-        print(is_up, response_time)
+        data = check(url, caching_enabled)
+        store(data, database)
         time.sleep(frequency)
 
 if __name__ == '__main__':
